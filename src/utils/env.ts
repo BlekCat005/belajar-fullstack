70ab3821a2b20dc8900a46c5ccd35da77955c208
Muhamad Rizki <siapasiapa005@gmail.com>
2025-06-04 15:35:16 +0700

    Try fix error vercel

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 const PORT = Number(process.env.PORT) || 3000;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/";
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SECRET = process.env.JWT_SECRET || "";
 const SECRET = process.env.SECRET || "";
 
 export { PORT, MONGODB_URI, JWT_SECRET, SECRET };
