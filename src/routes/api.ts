e8d91c6362d6f03d21da5db8bfa8259ef2b16111
Muhamad Rizki <siapasiapa005@gmail.com>
2025-06-13 13:58:49 +0700

    fix: api.ts

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -7,26 +7,56 @@ router.post(
   "/auth/register",
   authController.register
   /*
-  #swagger.tags = ['Auth']
-  #swagger.requestBody = {
-    required: true,
-    schema: {
-      $ref: "#/components/schemas/RegisterRequest"
-    }
-  }
+    #swagger.tags = ['Auth']
+    #swagger.summary = 'Mendaftarkan Pengguna Baru'
+    #swagger.description = 'Endpoint untuk membuat akun pengguna baru.'
+
+    #swagger.requestBody = {
+        required: true,
+        content: {
+            "application/json": {
+                schema: { $ref: "#/components/schemas/RegisterRequest" },
+                // INI BAGIAN PENTING YANG KITA TAMBAHKAN
+                examples: {
+                    ContohRegister: {
+                        value: { 
+                            fullname: "Budi Santoso",
+                            username: "budisan",
+                            email: "budi@example.com",
+                            password: "PasswordSuperAman123!" 
+                        }
+                    }
+                }
+            }
+        }
+    } 
   */
 );
+
 router.post(
   "/auth/login",
   authController.login
   /*
-  #swagger.tags = ['Auth']
-  #swagger.requestBody = {
-    required: true,
-    schema: {
-      $ref: "#/components/schemas/LoginRequest"
+    #swagger.tags = ['Auth']
+    #swagger.summary = 'Login Pengguna'
+    #swagger.description = 'Endpoint untuk autentikasi dan mendapatkan token.'
+    #swagger.requestBody = {
+        required: true,
+        content: {
+            "application/json": {
+                schema: { $ref: "#/components/schemas/LoginRequest" },
+                // DAN INI JUGA KITA TAMBAHKAN
+                examples: {
+                    ContohLogin: {
+                        value: {
+                            email: "user@example.com",
+                            password: "password123"
+                        }
+                    }
+                }
+            }
+        }
     }
-  }
   */
 );
 
